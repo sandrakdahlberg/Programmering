664c376ad7b924f844d0bcf1126b1e4b2fb67b38
Sandra Dahlberg <sandrakdahlberg@gmail.com>
2018-09-10 19:40:40 +0200

    Add project files.

---
+++ HelloGitHub/Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello GitHub"); 
+        }
+    }
+}
